e403ae13d12c127bd2dbc62cb81b71379e1a8e21
yashjawale <yash25.j@gmail.com>
2024-12-23 14:34:36 +0530

    Display product list on index.html

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # /products -> index
 def index(request):
-    return HttpResponse('Hello World')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
