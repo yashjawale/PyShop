b72f69912e546e43b801177d1e470ae5d89d916f
yashjawale <yash25.j@gmail.com>
2024-12-23 14:21:45 +0530

    Customize admin table columns

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+admin.site.register(Product, ProductAdmin)
