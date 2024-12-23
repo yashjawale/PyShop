9b4aa6bbff7b6a77a29c06878d52c75c7c464e54
yashjawale <yash25.j@gmail.com>
2024-12-23 13:53:56 +0530

    Add new task response

--- products/views.py
+++ products/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # /products -> index
 def index(request):
     return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New product route')
+
